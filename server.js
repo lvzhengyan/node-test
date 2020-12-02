7b9cdc0c5157a63e679cfb87eebab209dbc980a8
lv <1094749574@qq.com>
2020-12-02 23:35:50 +0800

    模拟请求访问css

--- server.js
+++ server.js
@@ -24,12 +24,25 @@ var server = http.createServer(function (request, response) {
     if (path === '/') {
         response.statusCode = 200
         response.setHeader('Content-Type', 'text/html;charset=utf-8')
-        response.write(`二哈`)
+        response.write(`
+        <!DOCTYPE html>
+        <html lang="zh-CN">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+            <link rel="stylesheet" href="/x">
+        </head>
+        <body>
+            <h1>我好了</h1>
+        </body>
+        </html>
+        `)
         response.end()
     } else if (path === '/x') {
         response.statusCode = 200
         response.setHeader('Content-Type', 'text/css;charset=utf-8')
-        response.write(`body{color: red;}`)
+        response.write(`h1{color: red;}`)
         response.end()
     } else {
         response.statusCode = 404
