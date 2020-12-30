ed078341235d49b64399e5d71beabd4c4c4a064b
lv <1094749574@qq.com>
2020-12-30 21:21:18 +0800

    修改server为静态服务器

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var url = require('url')
 var port = process.argv[2]
 
 if (!port) {
-    console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+    console.log('麻烦指定端口，像下面的格式一样\nnode server.js 8888')
     process.exit(1)
 }
 
@@ -19,39 +19,32 @@ var server = http.createServer(function (request, response) {
     var query = parsedUrl.query
     var method = request.method
 
-    console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
+    console.log('有请求过来啦！路径（带查询参数）为：' + pathWithQuery)
 
-    if (path === '/') {
-        response.statusCode = 200
-        response.setHeader('Content-Type', 'text/html;charset=utf-8')
-        response.write(`
-        <!DOCTYPE html>
-        <html lang="zh-CN">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
-            <link rel="stylesheet" href="/x">
-        </head>
-        <body>
-            <h1>我好了</h1>
-        </body>
-        </html>
-        `)
-        response.end()
-    } else if (path === '/x') {
-        response.statusCode = 200
-        response.setHeader('Content-Type', 'text/css;charset=utf-8')
-        response.write(`h1{color: red;}`)
-        response.end()
-    } else {
-        response.statusCode = 404
-        response.setHeader('Content-Type', 'text/html;charset=utf-8')
-        response.write(`你输入的路径不存在对应的内容`)
-        response.end()
+    response.statusCode = 200
+    // 默认首页
+    const filePath = path === '/' ? '/index.html' : path
+    const index = filePath.lastIndexOf('.')
+    const suffix = filePath.substring(index)
+    const fileTypes = {
+        '.html': 'text/html',
+        '.css': 'text/css',
+        '.js': 'text/javascript',
+        '.png': 'image/png',
+        '.jpg': 'image/jpeg'
     }
-
+    response.setHeader('Content-Type',
+        `${fileTypes[suffix] || 'text/html'};charset=utf-8`)
+    let content
+    try {
+        content = fs.readFileSync(`./public${filePath}`)
+    } catch (error) {
+        content = '您请求的路径不存在'
+        response.statusCode = 404
+    }
+    response.write(content)
+    response.end()
 })
 
 server.listen(port)
-console.log('监听 ' + port + ' 成功\n请用以下链接打开 http://localhost:' + port)
+console.log('监听 ' + port + ' 成功\n请用下面的链接打开 http://localhost:' + port)
